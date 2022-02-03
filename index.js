cd8bec6eae5e59523570c19678de8a3b5356bbb9
gluxe97 <96441367+gluxe97@users.noreply.github.com>
2022-02-02 21:11:36 -0600

    commit tree 47b52a02e56d3b6fd7d0baf24a7047ebc37e79a2

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function saturdayFun(par='roller-skate') {
+        return `This Saturday, I want to ${par}!`;
+    }
+function mondayWork(par="go to the office"){
+    return `This Monday, I will ${par}.`
+}
+function wrapAdjective(par1="*"){
+    const inner=function(par2="special"){
+        return `You are ${par1}${par2}${par1}!`;
+    }
+    return inner;
+}
